b9db6f1471f60c03d2a6ba1f5536a38f0bea2650
Timur Shatanov <58140315+shatanov@users.noreply.github.com>
2022-03-31 23:50:45 +0400

    add file to second work

---
+++ test.ps1
@@ -0,0 +1,18 @@
+﻿function findWord(){
+    $path = 'C:\test.txt'
+    $content = Get-Content -Path $path;
+    for($i = 0; $i -lt $content.Length; $i++){
+        $string = $content[$i];
+        $separators = @('.', ' ', ',', '/', '?', '!');
+        $words = $string.Split($separators);
+        foreach ($word in $words){
+            $maxLength = 0;
+            $maxWord = 
+            if($word.StartsWith($substring) -eq $true){
+                echo $word
+            }
+        }
+    }
+}
+
+findWord -substring 'te'
