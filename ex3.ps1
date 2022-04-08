0987436fee9de4ae177e3fa2a6fab1bfa990cf26
Timur Shatanov <58140315+shatanov@users.noreply.github.com>
2022-04-08 14:45:25 +0400

    Update ex3.ps1

--- ex3.ps1
+++ ex3.ps1
@@ -1,32 +1,44 @@
-function findWord(){
-    $path = 'C:\test.txt'
-    $content = Get-Content -Path $path;
-    for($i = 0; $i -lt $content.Length; $i++){
-        $arr = @()
-        $string = $content[$i];
-        $separators = @('.', ' ', ',', '/', '?', '!');
-        $words = $string.Split($separators);
-        $arr += $words
-        booleanFilter -text $arr -bool $true -argI 2;
-    }
-};
-
 function textIncludesFilter($text, $subStr, $argI){
-    $word = $text[$argI]
-    if($word.Contains($subStr) -eq $true){
+    if($subStr -eq ""){return $true}
+    $word = $text[$argI].ToLower()
+    if($word.Contains($subStr.ToLower()) -eq $true){
         return $true
     } else {
         return $false
     };
 };
 
-function numsFilter($text, $idx, $argI){
+function numsFilter($text, $numI, $argI){
+    if($numI -eq ""){return $true}
     $strNum = $text[$argI]
-    $nums = @()
-    
+    $num = [float]$strNum
+    $sep = "from-to"
+    if($numI[0] -eq "*"){
+       $sep = "to"
+     } else {
+         if($numI[$numI.Length - 1] -eq "*"){
+             $sep = "from"
+         }
+    }
+    $numArr = $numI.Split(@("*", " "))
+    if($sep -eq "to" -and $num -le $numArr[0]){
+        return $true
+    }
+
+    if($sep -eq "from" -and $num -ge $numArr[0]){
+        return $true
+    }
+
+    if($sep -eq "from-to" -and $num -le $numArr[1] -and $num -ge $numArr[0]){
+        return $true
+    }
+
+    return $false 
+
 };
 
 function booleanFilter($text, $bool, $argI){
+   if($bool -eq ""){return $true}
    $word = $text[$argI]
    if($word -eq $bool){
        return $true
@@ -36,4 +48,31 @@ function booleanFilter($text, $bool, $argI){
 
 };
 
-findWord
+function filterText($f1, $f2, $f3, $f4, $f5, $f6){
+    $path = 'C:\Users\tshatanov\Desktop\test1.txt'
+    $content = Get-Content -Path $path -Encoding UTF8;
+    $f1 = $f1.Split("prm1=")
+    $f2 = $f2.Split("prm2=")
+    $f3 = $f3.Split("prm3=")
+    $f4 = $f4.Split("prm4=")
+    $f5 = $f5.Split("prm5=")
+    $f6 = $f6.Split("prm6=")
+    echo $f1
+    for($i = 0; $i -lt $content.Length; $i++){
+        $string = $content[$i];
+        $string = $string -replace "---", '!';
+        $text = $string.Split("!")
+        $col1 = numsFilter -text $text -numI $f1[0] -argI 0;
+        $col2 = textIncludesFilter -text $text -subStr $f2[0] -argI 1;
+        $col3 = textIncludesFilter -text $text -subStr $f3[0] -argI 2;
+        $col4 = numsFilter -text $text -numI $f4[0] -argI 3;
+        $col5 = textIncludesFilter -text $text -subStr $f5[0] -argI 4;
+        $col6 = textIncludesFilter -text $text -subStr $f6[0] -argI 5;
+        if(($col1) -eq $true -and ($col2) -eq $true -and ($col3) -eq $true -and ($col4) -eq $true -and ($col5) -eq $true -and ($col6) -eq $true){
+            echo $string
+        }
+    }
+
+};
+
+filterText $args[0] $args[1] $args[2] $args[3] $args[4] $args[5]
