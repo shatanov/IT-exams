253580c87f5b5f9eb2db864b009d15f26569b9eb
Timur Shatanov <58140315+shatanov@users.noreply.github.com>
2022-04-21 19:15:40 +0300

    Update ex3.ps1

--- ex3.ps1
+++ ex3.ps1
@@ -40,40 +40,27 @@ function numsFilter($text, $numI, $argI){
 
 };
 
-function booleanFilter($text, $bool, $argI){
-   if($bool -eq ""){return $true}
-   $word = $text[$argI]
-   if($word -eq $bool){
-       return $true
-   } else {
-       return $false 
-   };
 
-};
-
-function filterText($f1, $f2, $f3, $f4, $f5, $f6){
+function filterText($f1, $f2, $f3, $f4, $f5){
     $content = Get-Content -Path 'C:\test.txt' -Encoding UTF8;
     $f1 = $f1.Split("=")
     $f2 = $f2.Split("=")
     $f3 = $f3.Split("=")
     $f4 = $f4.Split("=")
     $f5 = $f5.Split("=")
-    $f6 = $f6.Split("=")
     for($i = 0; $i -lt $content.Length; $i++){
         $string = $content[$i];
-        $string = $string -replace "---", '!';
         $text = $string.Split("!")
-        $col1 = numsFilter -text $text -numI $f1[1] -argI 0;
+        $col1 = textIncludesFilter -text $text -subStr $f1[1] -argI 0;;
         $col2 = textIncludesFilter -text $text -subStr $f2[1] -argI 1;
-        $col3 = textIncludesFilter -text $text -subStr $f3[1] -argI 2;
-        $col4 = numsFilter -text $text -numI $f4[1] -argI 3;
+        $col3 = numsFilter -text $text -numI $f3[1] -argI 2;
+        $col4 = textIncludesFilter -text $text -subStr $f4[1] -argI 3;
         $col5 = textIncludesFilter -text $text -subStr $f5[1] -argI 4;
-        $col6 = textIncludesFilter -text $text -subStr $f6[1] -argI 5;
-        if(($col1) -eq $true -and ($col2) -eq $true -and ($col3) -eq $true -and ($col4) -eq $true -and ($col5) -eq $true -and ($col6) -eq $true){
+        if(($col1) -eq $true -and ($col2) -eq $true -and ($col3) -eq $true -and ($col4) -eq $true -and ($col5) -eq $true){
             echo $string
         }
     }
 
 };
 
-filterText $args[0] $args[1] $args[2] $args[3] $args[4] $args[5]
+filterText $args[0] $args[1] $args[2] $args[3] $args[4]
