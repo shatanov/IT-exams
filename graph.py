72d5dda3e959738bf964a3607052cd596d6749c4
Timur Shatanov <58140315+shatanov@users.noreply.github.com>
2022-05-18 21:07:00 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -5,47 +5,11 @@ def read_csv_file(file: str):
     df = pd.read_csv(file)
 
     x = df['X']
-    y1 = df['Y1']
-    y2 = df['Y2']
-    y3 = df['Y3']
-    y4 = df['Y4']
-    y5 = df['Y5']
-    y6 = df['Y6']
-    y7 = df['Y7']
-    y8 = df['Y8']
-    y9 = df['Y9']
-    y10 = df['Y10']
+    y = df['Y']
     plt.style.use('_mpl-gallery')
     fig, ax = plt.subplots()
-    ax.scatter(x, y1,
+    ax.scatter(x, y,
                c='r',
-               s = 1)
-    ax.scatter(x, y2,
-               c='g',
-               s = 1)
-    ax.scatter(x, y3,
-               c='c',
-               s = 1)
-    ax.scatter(x, y4,
-               c='m',
-               s = 1)
-    ax.scatter(x, y5,
-               c='y',
-               s = 1)
-    ax.scatter(x, y6,
-               c='k',
-               s = 1)
-    ax.scatter(x, y7,
-               c='w',
-               s = 1)
-    ax.scatter(x, y8,
-               c='#ad09a3',
-               s = 1)
-    ax.scatter(x, y9,
-               c='b',
-               s = 1)
-    ax.scatter(x, y10,
-               c=[[0.1, 0.63, 0.55]],
                s = 1)
     ax.set_facecolor('black')
 
